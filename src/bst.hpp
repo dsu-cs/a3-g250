54e19ad5411e6c9649827348cd256ef4f3e2e627
g250 <46473037+g250@users.noreply.github.com>
2019-11-07 14:30:51 -0500

    added funcs

--- src/bst.hpp
+++ src/bst.hpp
@@ -35,11 +35,13 @@ public:
     // Gets the current number of nodes in the tree
     // returns: the number of nodes in the tree
     int get_size(void);
+    
 private:
     // the root node of the tree
     Node<T> *root;
     // the number of nodes in the tree
     int node_count;
+    //Node<T> *inserthelper(T);
 };
 
 template<class T>
@@ -60,10 +62,20 @@ BST<T>::~BST()
 }
 
 template<class T>
- std::vector<T> * BST<T>::inorder()
+  std::vector<T> * BST<T>::inorder()
 {
     std::vector<T> *vec = new std::vector<T>;
-
+    
+    if( root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        /*inorder(root->get_left());
+        cout << root->get_data << " "; 
+        inorder(root->get_right());*/
+    }
     return vec;
 }
 
@@ -72,6 +84,17 @@ template<class T>
  std::vector<T> * BST<T>::preorder()
 {
     std::vector<T> *vec = new std::vector<T>;
+
+     if( root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        /*cout << root->get_data << " "; 
+        preorder(root->get_left());
+        preorder(root->get_right());*/
+    }
     return vec;
 }
 
@@ -81,19 +104,79 @@ template<class T>
 {
     std::vector<T> *vec = new std::vector<T>;
 
+     if( root == NULL)
+    {
+        return 0;
+    }
+    else
+    {   
+        /*postorder(root->get_left());
+        postorder(root->get_right());
+        vec = root->get_data << " "; */
+    }
+    
+
     return vec;
 }
 
 template<class T>
 void BST<T>::insert(T new_data)
 {
-
+    root = inserthelper(new_data, root);    
 }
+template<class T>
+Node<T> *BST<T>::inserthelper(T new_data, Node<T>* node)
+{
+    Node<T> *tmp;
+    Node<T> *tmp2;
 
+    if(root == NULL)
+    {
+        tmp->set_data(new_data);
+        tmp->set_left(NULL);
+        tmp->set_right(NULL);
+        return tmp;
+    }
+    else 
+    {
+        
+        if(tmp->get_data() > new_data)
+        {
+            root->set_left(inserthelper( new_data, root->get_left()));
+
+            
+        }
+        else
+        {
+          
+        }
+    
+}
 
 template<class T>
 Node<T> *BST<T>::search(T val)
 {
+     if( root == NULL)
+    {
+        return NULL;
+    }
+    else if( root->get_data() == val)
+    {
+        return root->get_data();
+    }
+    else
+    {
+        if(root->get_data > val)
+        {
+            return search(root->get_left(), val);
+        }
+        else(root->get_data < val)
+        {
+            return search(root->get_right(), val);
+        }
+        
+    }
+    
 
 }
 
@@ -102,13 +185,77 @@ Node<T> *BST<T>::search(T val)
 template<class T>
 void BST<T>::remove(T val)
 {
+    
 
+    if(root == NULL)
+    {
+        return;
+    }
+    else if(root->get_data() > val)
+    {
+        root->set_left(remove(root->get_left(), val));
+    }
+    else if(root->get_data() < val)
+    {
+        root->set_right(remove(root->get_right(), val));
+    }
+    else
+    {
+        if(root->get_left() == NULL && root->get_right() == NULL)
+        {
+            delete root;
+            root = NULL;
+            
+        }
+        else if(root->get_left() == NULL)
+        {
+            Node<T> *tmp = root;
+            root = root->get_right();
+            delete tmp;
+            
+
+        }
+        else if(root->get_right() == NULL)
+        {
+            Node<T> *tmp2 = root;
+            root = root->get_left();
+            delete tmp;
+            
+
+        }
+        else
+        {
+            Node<T> *tmp3 = findmin(root->get_right());
+            root = tmp3;
+            root->set_right(remove(root->get_right(), tmp3->get_data()));
+        }
+        
+    }
+    
 }
-
+template<class T>
+int BST<T>::findmin(Node *root)
+{
+   if (root == NULL) {
+      return NULL;
+   }
+   if (root->get_left() != NULL) {
+      return FindMin(root->get_left());
+   }
+   return root->get_data();
+}
 
 
 template<class T>
 int BST<T>::get_size()
 {
-
+     if(root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        
+    }
+    
 }
